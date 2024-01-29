5bd02f27321137354751cf9a4b41b24c300e1962
insightSush <kainths@algonquincollege.com>
2024-01-28 23:39:10 -0500

    issue-1: setting up authenticate

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import {useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {logOut} from "../redux/slicers/authSlicer";
 
 export default function () {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     return <>
         hell yeah!!
+        <button onClick={() => {
+            dispatch(logOut())
+            navigate('/authenticate')
+        }}>Logout</button>
     </>
 }
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -4,23 +4,27 @@ import Login from "../components/authentication/Login";
 import {useNavigate} from "react-router-dom";
 import {useValidateTokenQuery} from "../redux/services/authAPI";
 import CustomSpinner from "../components/util/CustomSpinner";
+import {useDispatch} from "react-redux";
+import {logOut} from "../redux/slicers/authSlicer";
 
 export default function () {
-    // const navigate = useNavigate();
-    //
-    // if (localStorage.getItem('access')) {
-    //     const {
-    //         data: isValidToken,
-    //         isLoading,
-    //         error
-    //     } = useValidateTokenQuery(localStorage.getItem('access'));
-    //     if (isLoading) {
-    //         return <CustomSpinner text={'Validating..'}/>
-    //     }else if (error) {
-    //         console.error(error)
-    //     }else if (isValidToken) {
-    //         navigate('/')
-    //     }
-    // }
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    if (localStorage.getItem('access')) {
+        const {
+            data: isValidToken,
+            isLoading,
+            error
+        } = useValidateTokenQuery(localStorage.getItem('access'));
+        if (isLoading) {
+            return <CustomSpinner />;
+        }else if (error) {
+            dispatch(logOut())
+            console.error(error);
+        }else if (isValidToken) {
+            navigate('/');
+        }
+    }
     return <Login />
 }
