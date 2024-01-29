e512569d8055e8f9759c233690b538736a668a0a
insightSush <kainths@algonquincollege.com>
2024-01-29 01:24:13 -0500

    issue-1: registering user without any role

---
+++ src/components/authentication/RegisterUser.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import {Button, Form, Input} from "antd";
+import {useRegisterMutation} from "../../redux/services/authAPI";
+import {useDispatch} from "react-redux";
+import {setCredentials} from "../../redux/slicers/authSlicer";
+import {useNavigate} from "react-router-dom";
+
+export default function ({
+                             setCurrentState,
+                         }) {
+
+    const [form] = Form.useForm()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const rules = [{
+        required: true,
+        message: 'The field is required.'
+    }]
+
+    const [
+        registerUser,
+        {isLoading: isRegisteringUser}
+    ] = useRegisterMutation()
+
+    function onFinish() {
+        if (form.getFieldValue('password') !== form.getFieldValue('confirmPassword')) {
+            form.setFields([
+                {
+                    name: "confirmPassword",
+                    errors: ["Passwords do not match"]
+                },
+                {
+                    name: "password",
+                    errors: ["Passwords do not match"]
+                }
+            ]);
+            return ;
+        }
+
+        registerUser(form.getFieldsValue()).then(({data, error})=> {
+            if (data) {
+                dispatch(setCredentials({...data.data, username: form.getFieldValue('email')}))
+                setCurrentState(1)
+            } else {
+                console.error(error)
+            }
+        })
+
+
+    }
+
+    return (
+        <div style={registerUserStyles.container}>
+            <h1>Welcome!!</h1>
+            <p>We need a few details to get started.</p>
+            <Form
+                form={form}
+                layout={"vertical"}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name={'name'}
+                >
+                    <Input placeholder={'Full name'} style={registerUserStyles.input}/>
+
+                </Form.Item>
+                <Form.Item
+                    name={'email'} rules={[
+                    {
+                        required: true,
+                        message: 'The field is required.'
+                    },
+                    {
+                        type: "email",
+                        message: "Invalid email address format."
+                    }
+                ]}>
+                    <Input placeholder={'Email'} style={registerUserStyles.input}/>
+                </Form.Item>
+                <Form.Item name={'password'} rules={rules}>
+                    <Input.Password
+                        visibilityToggle={false}
+                        placeholder={'Password'} style={registerUserStyles.input}/>
+                </Form.Item>
+                <Form.Item name={'confirmPassword'} rules={rules}>
+                    <Input
+                        placeholder={'Confirm Password'} style={registerUserStyles.input}/>
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        style={{
+                            ...registerUserStyles.buttons,
+                            color: '#ece7e2',
+                            textShadow: '0 1px 3px'
+                        }}
+                        loading={isRegisteringUser}
+                        htmlType={'submit'}
+                    >
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    )
+
+}
+
+const registerUserStyles = {
+    container: {
+        color: '#f2f2f2',
+        padding: '10px'
+    },
+    input: {
+        height: '58px',
+        color: '#ece7e2',
+        maxWidth: '450px',
+        fontWeight: 500,
+        fontSize: '16px',
+        border: 'none',
+        boxShadow: 'none',
+        background: 'none'
+
+    },
+    buttons: {
+        width: '240px',
+        height: '50px',
+        borderRadius: '10px',
+        fontWeight: 'bolder',
+        fontSize: '25px',
+        background: 'none',
+        border: 'none',
+        boxShadow: '0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19)'
+
+    }
+}
--- src/screens/RegistrationScreen.jsx
+++ src/screens/RegistrationScreen.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
-import CustomSpinner from "../components/util/CustomSpinner";
+import React, {useState} from 'react'
+import RegisterUser from "../components/authentication/RegisterUser";
+import RegisterBusiness from "../components/authentication/RegisterBusiness";
+import {Col, Image, Row} from "antd";
+import useBreakpoint from "antd/es/grid/hooks/useBreakpoint";
 
 export default function RegistrationScreen() {
+    const [currentState, setCurrentState] = useState(0);
+    const screens = useBreakpoint();
+    const secondDivsHeight = screens.md || screens.lg || screens.xl || screens.xxl ? '100vh' : '88vh';
 
     return (
-        <>
-            <CustomSpinner />
-        </>
+        <Row style={registerScreenStyles.mainContainer}>
+            <Col xl={8} md={8} sm={24} xs={24}
+                 style={registerScreenStyles.innerDivs}
+            >
+                <Image
+                    style={{
+                        maxHeight: '120px',
+                        padding: '10px'
+                    }}
+                    preview={false}
+                    src={require('../assets/img/GetLocals-logos/GetLocals-logos_transparent_inverse.png')}/>
+            </Col>
+            <Col xl={16} md={16} sm={24} xs={24}
+                 style={{
+                     backgroundColor: 'var(--primary-color)',
+                     minHeight: secondDivsHeight,
+
+                     ...registerScreenStyles.innerDivs
+                 }}
+            >
+                {currentState === 0 ?
+                    <RegisterUser
+                        setCurrentState={setCurrentState}
+                    /> :
+                    <RegisterBusiness
+                        setCurrentState={setCurrentState}
+                    />
+                }
+            </Col>
+
+        </Row>
     )
 
+}
+
+const registerScreenStyles = {
+
+    mainContainer : {
+        margin: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh'
+    },
+    innerDivs: {
+        display: 'inline-block',
+        verticalAlign: 'middle',
+        textAlign: 'center',
+        alignItems: 'center',
+    },
 }
